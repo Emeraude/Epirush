48a6bda25931009249cedee14b49c8a74867499b
bouche_1 <bouche_1@epitech.eu>
2013-12-13 21:39:52 +0100

    MAINTENANT OUI

--- decode/decode.c
+++ decode/decode.c
@@ -5,17 +5,88 @@
 ** Login   <bouche_1@epitech.net>
 ** 
 ** Started on  Fri Dec 13 19:06:11 2013 bouche_1
-** Last update Fri Dec 13 19:13:28 2013 bouche_1
+** Last update Fri Dec 13 21:33:41 2013 bouche_1
 */
 
-int	main(int argc, char **argv)
+#include "header.h"
+#include "../my_str.h"
+
+void	my_translator_alpha(char *str)
+{
+  my_putstr(str);
+}
+void	my_dispatcher(char *str, int line_length)
+{
+  if (line_length > 4)
+    my_translator_numeric(str);
+  else
+    my_translator_alpha(str);
+}
+
+void	my_less(char *str)
 {
   int	i;
 
-  i = 1;
-  while (argv[i] != '\0')
+  i = 0;
+  while (str[i] == '-')
     {
+      i = i + 1;
+    }
+  my_putchar(i + '0');
+}
 
+void	my_more(char *str)
+{
+  int	i;
+
+  i = 0;
+  while (str[i] == '_')
+    {
+      i = i + 1;
+    }
+  my_putchar((i + 5) + '0');
+}
+
+void	my_translator_numeric(char *str)
+{
+  if (str[0] == '-')
+    my_less(str);
+  else if (str[0] == '_' && str[4] == '-')
+    my_more(str);
+  else if (str[0] == '_' && str[4] == '_')
+    my_putchar('0');
+}
+
+int	my_verif_and_count(char *str)
+{
+  int	i;
+
+  i = 0;
+  while (str[i] != '\0')
+    {
+      if (str[i] != '-' && str[i] != '_' && str[i] != '.' && str[i] != '\0')
+	{
+	  my_puterr("TROLOL T TRO NUL\n");
+	  exit(EXIT_FAILURE);
+	}
+      i = i + 1;
+    }
+  my_putstr("-- It passed ! WELL DONE ! --\n");
+  return (i);
+}
+
+int	main(void)
+{
+  char	*str;
+  int	line_length;
+
+  line_length = 0;
+  while ((str = get_next_line(0)) != NULL)
+    {
+      line_length = my_verif_and_count(str);
+      my_dispatcher(str, line_length);
+      free(str);
+      my_putchar('\n');
     }
   return (0);
 }
--- decode/header.h
+++ decode/header.h
@@ -5,12 +5,21 @@
 ** Login   <bouche_1@epitech.net>
 ** 
 ** Started on  Fri Dec 13 19:21:55 2013 bouche_1
-** Last update Fri Dec 13 19:22:22 2013 bouche_1
+** Last update Fri Dec 13 21:11:51 2013 bouche_1
 */
 
 #ifndef HEADER_H_
 # define HEADER_H_
 
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "../get_next_line.h"
+# include "../my_str.h"
 
+int	my_verif_and_count(char *str);
+void	my_translator_numeric(char *str);
+void	my_dispatcher(char *str, int line_length);
 
 #endif /* !HEADER_H_ */
