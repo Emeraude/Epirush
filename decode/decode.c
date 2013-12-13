096a34b069123d29a902c0121a902a3d85e34c44
bouche_1 <bouche_1@epitech.eu>
2013-12-13 19:29:22 +0100

    Deco v0.0

---
+++ decode/decode.c
@@ -0,0 +1,21 @@
+/*
+** decode.c for DECODE in /home/bouche_1/TESTS_INTERNE/rush/decode
+** 
+** Made by bouche_1
+** Login   <bouche_1@epitech.net>
+** 
+** Started on  Fri Dec 13 19:06:11 2013 bouche_1
+** Last update Fri Dec 13 19:13:28 2013 bouche_1
+*/
+
+int	main(int argc, char **argv)
+{
+  int	i;
+
+  i = 1;
+  while (argv[i] != '\0')
+    {
+
+    }
+  return (0);
+}
---
+++ decode/header.h
@@ -0,0 +1,16 @@
+/*
+** header.h for HEADER in /home/bouche_1/TESTS_INTERNE/rush/decode
+** 
+** Made by bouche_1
+** Login   <bouche_1@epitech.net>
+** 
+** Started on  Fri Dec 13 19:21:55 2013 bouche_1
+** Last update Fri Dec 13 19:22:22 2013 bouche_1
+*/
+
+#ifndef HEADER_H_
+# define HEADER_H_
+
+
+
+#endif /* !HEADER_H_ */
