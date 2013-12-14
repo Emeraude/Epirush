24ff731492ad2078261ac11872d53c29b1d2bbd2
broggi_t <broggi_t@epitech.eu>
2013-12-14 02:55:32 +0100

    adding my_putcharerr

--- my_str.c
+++ my_str.c
@@ -5,7 +5,7 @@
 ** Login   <broggi_t@epitech.net>
 ** 
 ** Started on  Fri Dec 13 19:49:21 2013 broggi_t
-** Last update Fri Dec 13 19:51:19 2013 broggi_t
+** Last update Sat Dec 14 02:54:49 2013 broggi_t
 */
 
 #include <unistd.h>
@@ -13,6 +13,11 @@
 void		my_putchar(char c)
 {
   write(1, &c, 1);
+}
+
+void		my_putcharerr(char c)
+{
+  write(2, &c, 1);
 }
 
 unsigned int	my_strlen(char *str)
--- my_str.h
+++ my_str.h
@@ -5,13 +5,14 @@
 ** Login   <broggi_t@epitech.net>
 ** 
 ** Started on  Fri Dec 13 19:51:25 2013 broggi_t
-** Last update Fri Dec 13 19:52:12 2013 broggi_t
+** Last update Sat Dec 14 02:55:04 2013 broggi_t
 */
 
 #ifndef MY_STR_H_
 # define MY_STR_H_
 
 void		my_putchar(char c);
+void		my_putcharerr(char c);
 unsigned int	my_strlen(char *str);
 void		my_putstr(char *str);
 void		my_puterr(char *str);
